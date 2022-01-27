3867a99e21ecf23f388e518dd4e70b750b33cd82
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-01-27 13:55:57 -0600

    enriqueciendo la informacion

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
+const baseUrl = "https://platzi-avo.vercel.app";
 const url = "https://platzi-avo.vercel.app/api/avo";
-
+const appNode = document.querySelector('#app');
 //web api
 //conectamos al server 
 //promise -> async/await
 window
-    .fetch(url)
+    .fetch(`${baseUrl}/api/avo`)
     //procesar la respuesta y convertirla en JSON
     .then((respuesta) => respuesta.json())
     //Json -> Data -> Renderizar info browser
@@ -21,11 +22,12 @@ window
 
             //crear titulo
             const title = document.createElement("h2");
-
+            title.textContent = item.name;
+            imagen.src = `${baseUrl}${item.image}`;
 
             //crear precio
             const price = document.createElement("div");
-
+            price.textContent = item.price;
 
             const container = document.createElement("div");
             container.append(imagen, title, price);
@@ -33,5 +35,5 @@ window
             todosLosItems.push(container);
 
         });
-        document.body.append(...todosLosItems);
+        appNode.append(...todosLosItems);
     });
