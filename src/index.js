59aa3d2e28a203b9d55345e8fa130a52900b2af9
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-01-27 19:19:51 -0600

    añadiendo css e internacionalizacion

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@
 const baseUrl = "https://platzi-avo.vercel.app";
 const url = "https://platzi-avo.vercel.app/api/avo";
 const appNode = document.querySelector('#app');
+const formatPrice = (price) => {
+
+        const newPrice = new window.Intl.NumberFormat("en-ES", {
+            style: "currency",
+            currency: "MXN",
+        }).format(price)
+        return newPrice;
+    }
+    //intl
+    //formato de monedas
+
+
+
 //web api
 //conectamos al server 
 //promise -> async/await
@@ -19,15 +32,20 @@ window
         responseJson.data.forEach((item) => {
             //crear imagen
             const imagen = document.createElement("img");
+            imagen.className = "imagen-acomodo";
+
 
             //crear titulo
             const title = document.createElement("h2");
             title.textContent = item.name;
             imagen.src = `${baseUrl}${item.image}`;
+            title.className = "texto-acomodo";
 
             //crear precio
             const price = document.createElement("div");
             price.textContent = item.price;
+            price.className = "texto-acomodo-price";
+            price.textContent = formatPrice(item.price);
 
             const container = document.createElement("div");
             container.append(imagen, title, price);
